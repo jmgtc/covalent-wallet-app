465ef3ebd734c63211a3d5317dd68fc0942aeea2
John Grimm <jmgtc@protonmail.com>
2020-12-16 15:45:15 +0100

    Style the BalanceItems

--- src/Root/Body/BalanceItem/BalanceItem.tsx
+++ src/Root/Body/BalanceItem/BalanceItem.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import { Contract } from '../Body';
 import styles from './BalanceItem.module.scss';
-import { Card, Elevation } from '@blueprintjs/core';
+import { Card, Elevation, Tooltip } from '@blueprintjs/core';
 
 export const BalanceItem: React.FC<{ item: Contract }> = ({ item }) => {
+  const quoteMajor = Math.floor(item.quote);
+  const quoteMinor = (item.quote - quoteMajor).toFixed(18).substring(1);
+
+  const TooltipContent = <div>{item.contract_name}</div>;
+
   return (
     <Card className={styles.root} elevation={Elevation.ZERO}>
       <div className={styles.balanceInfo}>
-        <div className={styles.logo}>
-          <img
-            className={styles.logoImg}
-            src={item.logo_url}
-            alt={item.contract_name}
-          />
+        <Tooltip className={styles.tooltip} content={TooltipContent}>
+          <div className={styles.balance}>
+            <div className={styles.logo}>
+              <img
+                className={styles.logoImg}
+                src={item.logo_url}
+                alt={item.contract_name}
+              />
+            </div>
+            <div className={styles.symbol} title={item.contract_name}>
+              {item.contract_ticker_symbol}
+            </div>
+          </div>
+        </Tooltip>
+        <div className={styles.quote}>
+          <span className={styles.quoteMajor}>{quoteMajor}</span>
+          <span className={styles.quoteMinor}>{quoteMinor}</span>
         </div>
-        <div title={item.contract_name}>{item.contract_ticker_symbol}</div>
-        <div>{item.quote}</div>
-        {/* <div>{item.quote_rate}</div> */}
+        <div className={styles.fiat}>~${item.quote_rate}</div>
       </div>
-      {/* <div>{item.type}</div> */}
     </Card>
   );
 };
