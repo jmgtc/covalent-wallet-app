ad5fde17b52085742b50018288cbf05e9a4f037a
johnmgrimm <jmgtc@protonmail.com>
2020-12-17 00:29:04 +0100

    Improve footer

--- src/Root/Footer/Footer.tsx
+++ src/Root/Footer/Footer.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import styles from './Footer.module.scss';
+import logo from '../../assets/images/covalent-logo.png';
 
 export const Footer: React.FC = () => {
-  return <div className={styles.root}>Covallet</div>;
+  return (
+    <div className={styles.root}>
+      <span>Powered by</span>
+      <img className={styles.logoImg} src={logo} alt="Covalent" />
+    </div>
+  );
 };
