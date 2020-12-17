6f7cdfaf2ad5fc4b6a8ae5f555439d6a313e0a10
johnmgrimm <jmgtc@protonmail.com>
2020-12-17 19:38:30 +0100

    Link footer

--- src/Root/Footer/Footer.tsx
+++ src/Root/Footer/Footer.tsx
@@ -5,8 +5,15 @@ import logo from '../../assets/images/covalent-logo.png';
 export const Footer: React.FC = () => {
   return (
     <div className={styles.root}>
-      <span>Powered by</span>
-      <img className={styles.logoImg} src={logo} alt="Covalent" />
+      <a
+        className={styles.link}
+        href="https://www.covalenthq.com/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <span>Powered by</span>
+        <img className={styles.logoImg} src={logo} alt="Covalent" />
+      </a>
     </div>
   );
 };
